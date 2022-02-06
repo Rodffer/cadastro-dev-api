960aeac9614ae4779c057b4ca213708e2e78114f
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-06 20:38:17 -0300

    Feat: olá dev

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log('-------------------')
+  console.log('Banco de dados conectado');
+  console.log('-------------------')
+});
 
 const app = express();
 
+// middlewares globais
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Olá Dev'});
+});
+
 app.listen(8800, () => {
   console.log('-------------------')
-  console.log('Backend Iniciado');
+  console.log('Backend Iniciado 🔥');
   console.log('-------------------')
-})
+});
+
