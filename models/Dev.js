3ae17e4a12124c562f42cd714c79ce076472eade
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-06 21:14:47 -0300

    Feat: adicionado model do dev

---
+++ models/Dev.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const DevSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    require: true,
+    max: 50
+  },
+  lastName: {
+    type: String,
+    require: true,
+    max: 50
+  },
+  email: {
+    type: String,
+    require: true,
+    max: 50,
+    unique: true
+  },
+  role: {
+    type: String,
+    require: true,
+    enum: ['frontend', 'backend', 'fullstack'],
+    require: true
+  },
+  levels: {
+    type: String,
+    require: true,
+    enum: ['junior', 'middle', 'senior'],
+    require: true
+  },
+  stacks: {
+    type: Array,
+    default: [],
+  },
+  description: {
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Dev', DevSchema);
--- routes/dev.js
+++ routes/dev.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
+/*
+
+  POST: Criar
+  GET: Buscar
+  PUT: Editar
+  PATCH: Editar / Apenas 1 informação
+  DELETE: Apagar
+
+  C reate - POST
+  R ead - GET
+  U update - PUT/PATCH
+  D elete - DELETE
+
+*/
+
+router.post('/', async (req, res) => {
   res.json({ message: 'Olá Dev'});
 });
 
